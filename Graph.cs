9f79e9501963fce7ed9d3823d0769c543c5e4302
Yacinekhier <76571828+Yacinekhier@users.noreply.github.com>
2022-12-17 21:48:44 +0100

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimsAlgoCSharp
+{
+        public class Graph
+        {
+            
+            int inf = Int32.MaxValue;
+
+            public void ACM(int nbSommet, int[,] matAdj, Dictionary<int, string> sommet)
+            {
+
+                bool[] selected = new bool[nbSommet];
+
+                int nbArcs = 0;
+
+                Console.WriteLine("L'arbre couvrant de poids minimal:\n\n");
+                Console.WriteLine("  Arc: \t\t Poids:");
+                Console.WriteLine();
+
+                // first vertex is selected
+                selected[0] = true;
+                while (nbArcs < nbSommet - 1)
+                {
+                    int min = inf;
+                    int x = 0, y = 0;
+                    
+                    for (int i = 0; i < nbSommet; i++)
+                    {
+                        // 
+                        if (selected[i])
+                        {
+                            for (int j = 0; j < nbSommet; j++)
+                            {
+                            // 
+                            if ((!selected[j]) && matAdj[i, j] != 0)
+                                {
+                                    // 
+                                    if (min > matAdj[i, j])
+                                    {
+                                        //
+                                        min = matAdj[i, j];
+                                        x = i;
+                                        y = j;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    // 
+                    Console.WriteLine(" (" + sommet[x] + "-" + sommet[y] + ")  \t" + matAdj[x, y]);
+                    Console.WriteLine();
+                    // 
+                    selected[y] = true;
+                    nbArcs += 1;
+
+                }
+            }
+
+
+
+        }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimsAlgoCSharp
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+
+            // Adjacency matrix of G
+            int[,] matAdj = new int[,]{{0,  15, 12, 9,  0, 0,  0},
+                                       {15, 0,  0,  16, 0, 0,  0},
+                                       {12, 0,  0,  7,  6, 0,  0},
+                                       {9,  16, 7,  0,  0, 13, 0},
+                                       {0,  0,  6,  0,  0, 5,  8},
+                                       {0,  0,  0,  13, 5, 0,  0},
+                                       {0,  0,  0,  0,  8, 0,  0}};
+            // 
+            Dictionary<int, string> sommet = new Dictionary<int, string>{
+                    { 0, "A" },
+                    { 1, "B" },
+                    { 2, "C" },
+                    { 3, "D" },
+                    { 4, "E" },
+                    { 5, "F" },
+                    { 6, "G" }
+                };
+
+            //
+            Graph G = new Graph();
+            // 
+            G.ACM(7, matAdj, sommet);
+            Console.ReadKey();
+        }
+    }
+}
